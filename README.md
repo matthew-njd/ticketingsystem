4b472447efaffc4570f467a699b3d76e06e2e9eb
mattyydagz <mnjdagostino@gmail.com>
2023-10-08 10:57:07 -0400

    init setup

---
+++ README.md
@@ -0,0 +1,6 @@
+Ticketing System Application
+
+Primary Goal: Users (role) can login and create, edit, and (possibly) delete tickets. Admins (role) will be able to review and update the status of current tickets.
+
+October 8th, 2023
+Initial setup - Supabase and Skeleton UI.
---
+++ src/lib/supabase.ts
@@ -0,0 +1,7 @@
+// import { createClient } from "@supabase/supabase-js";
+// import type { Database } from "$lib/types";
+
+// import { SUPABASE_URL } from "$env/static/private";
+// import { SUPABASE_ANON_KEY } from "$env/static/private";
+
+// export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_ANON_KEY);
